49d4b6470770d7bab533ffbf7c332ca98267ddf5
Skaade <Frederik.skaade@gmail.com>
2022-03-28 13:27:40 +0200

    Model lag

--- Aarhus Bryghus/src/application/controller/Controller.java
+++ Aarhus Bryghus/src/application/controller/Controller.java
@@ -1,10 +1,5 @@
 package application.controller;
 
 public class Controller {
-    public static void main(String[] args) {
-        System.out.println("Hej");
-        System.out.println("Hej igen");
+
     }
-
-
-}
