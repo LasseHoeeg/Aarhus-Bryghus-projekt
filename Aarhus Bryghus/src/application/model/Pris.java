e8137dbc0ec4e875e47c3aa5c17ff8166366cd95
Lasse Høeg Jensen <eaalashj@students.eaaa.dk>
2022-03-29 08:18:35 +0200

    hej

--- Aarhus Bryghus/src/application/model/Pris.java
+++ Aarhus Bryghus/src/application/model/Pris.java
@@ -1,4 +1,27 @@
 package application.model;
 
 public class Pris {
+    private double beloeb;
+    private int antalKlip;
+
+    public Pris(double beloeb, int antalKlip) {
+        this.beloeb = beloeb;
+        this.antalKlip = antalKlip;
+    }
+
+    public double getBeloeb() {
+        return beloeb;
+    }
+
+    public void setBeloeb(double beloeb) {
+        this.beloeb = beloeb;
+    }
+
+    public int getAntalKlip() {
+        return antalKlip;
+    }
+
+    public void setAntalKlip(int antalKlip) {
+        this.antalKlip = antalKlip;
+    }
 }
--- Aarhus Bryghus/src/application/model/Salgssituation.java
+++ Aarhus Bryghus/src/application/model/Salgssituation.java
@@ -1,4 +1,27 @@
 package application.model;
 
 public class Salgssituation {
+    private String navn;
+    private String deltagerInfo;
+
+    public Salgssituation(String navn, String deltagerInfo) {
+        this.navn = navn;
+        this.deltagerInfo = deltagerInfo;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getDeltagerInfo() {
+        return deltagerInfo;
+    }
+
+    public void setDeltagerInfo(String deltagerInfo) {
+        this.deltagerInfo = deltagerInfo;
+    }
 }
