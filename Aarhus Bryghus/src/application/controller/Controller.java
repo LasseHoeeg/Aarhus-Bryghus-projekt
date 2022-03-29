44c5e8e0caea6eb17e0cb9d73a1dddcc581d9b71
Skaade <Frederik.skaade@gmail.com>
2022-03-29 09:51:18 +0200

    Model lag

--- Aarhus Bryghus/src/application/controller/Controller.java
+++ Aarhus Bryghus/src/application/controller/Controller.java
@@ -1,5 +1,16 @@
 package application.controller;
 
-public class Controller {
+import storage.Storage;
 
+public class Controller {
+    private static Controller uniqueInstance;
+    private Storage storage;
+
+    public static Controller getInstance(){
+        if (uniqueInstance==null){uniqueInstance = new Controller();}
+        return uniqueInstance;
+    }
+
+    private Controller()
+    {storage=Storage.getInstance();}
     }
--- Aarhus Bryghus/src/storage/Storage.java
+++ Aarhus Bryghus/src/storage/Storage.java
@@ -1,4 +1,11 @@
 package storage;
 
 public class Storage {
+    private static Storage uniqueInstance;
+
+    public static Storage getInstance() {
+        if (uniqueInstance == null) {uniqueInstance = new Storage();
+        }
+        return uniqueInstance;
+    }
 }
