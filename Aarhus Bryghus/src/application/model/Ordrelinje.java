7abd7b7de329510910ddaeafabd63f6f74dc5e61
KristinaHK <kristina.h.k@hotmail.com>
2022-03-30 13:13:28 +0200

    Ordrelinje helt done (rabat alle steder noteret/kommenteret ud til senere)

--- Aarhus Bryghus/src/application/model/Ordrelinje.java
+++ Aarhus Bryghus/src/application/model/Ordrelinje.java
@@ -6,9 +6,39 @@ public class Ordrelinje {
     private Produkt produkt;
     private Rabat rabat;
 
-Ordrelinje(int nr, int antal, Produkt produkt){
-    this.nr=nr;
-    this.antal=antal;
+Ordrelinje(int nr, int antal, Produkt produkt){  //når rabat tilføj: , Rabat rabat){
+    this.nr = nr;
+    this.antal = antal;
+    this.produkt = produkt;
+    //this.rabat = rabat;
 }
+
+    public int getNr() {
+        return nr;
+    }
+
+    public void setNr(int nr) {
+        this.nr = nr;
+    }
+
+    public int getAntal() {
+        return antal;
+    }
+
+    public void setAntal(int antal) {
+        this.antal = antal;
+    }
+
+    public Produkt getProdukt() {
+        return produkt;
+    }
+
+
+    //Til når rabat kodes:
+
+//    public Rabat getRabat() {
+//        return rabat;
+//    }
+
 
 }
