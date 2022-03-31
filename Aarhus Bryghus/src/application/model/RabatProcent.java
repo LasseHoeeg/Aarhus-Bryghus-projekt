c6a47260b3b7945827acf9205e174a7f1bd7b16a
Lasse Høeg Jensen <eaalashj@students.eaaa.dk>
2022-03-31 09:41:38 +0200

    initStorage

--- Aarhus Bryghus/src/application/model/RabatProcent.java
+++ Aarhus Bryghus/src/application/model/RabatProcent.java
@@ -1,4 +1,21 @@
 package application.model;
 
-public class RabatProcent {
+public class RabatProcent extends Rabat {
+    private double procent;
+
+    public RabatProcent(double procent) {
+        this.procent = procent;
+    }
+
+    public double getRabat(double pris) {
+        return pris * procent / 100;
+    }
+
+    public double getProcent() {
+        return procent;
+    }
+
+    public void setProcent(double procent) {
+        this.procent = procent;
+    }
 }
