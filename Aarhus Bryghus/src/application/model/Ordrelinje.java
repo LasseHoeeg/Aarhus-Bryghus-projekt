55e9ab5ea6560931927eeeb954fc8cb264ab3213
Skaade <Frederik.skaade@gmail.com>
2022-03-31 10:49:50 +0200

    Skaades

--- Aarhus Bryghus/src/application/model/Ordrelinje.java
+++ Aarhus Bryghus/src/application/model/Ordrelinje.java
@@ -3,14 +3,24 @@ package application.model;
 public class Ordrelinje {
     private int nr;
     private int antal;
+    private double ordrelinjePris;
     private Produkt produkt;
     private Rabat rabat;
+    private Salg salg;
 
-Ordrelinje(int nr, int antal, Produkt produkt){  //når rabat tilføj: , Rabat rabat){
+
+    Ordrelinje(int nr, int antal, Produkt produkt, Salg salg){  //når rabat tilføj: , Rabat rabat){
     this.nr = nr;
     this.antal = antal;
     this.produkt = produkt;
+    this.salg = salg;
+    beregnOrdrelinjePris();
 }
+
+    public Salg getSalg() {
+        return salg;
+    }
+
 
     public Rabat getRabat() {
         return rabat;
@@ -54,4 +64,28 @@ Ordrelinje(int nr, int antal, Produkt produkt){  //når rabat tilføj: , Rabat r
         return produkt;
     }
 
+    public void setOrdrelinjePris(double ordrelinjePris) {
+        this.ordrelinjePris = ordrelinjePris;
+    }
+
+    public double getOrdrelinjePris() {
+        return ordrelinjePris;
+    }
+
+    public void beregnOrdrelinjePris() {
+        if (produkt==salg.getSalgssituation.getPris.getProdukt){
+            setOrdrelinjePris(salg.getSalgssituation.getPris*antal);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return produkt.getNavn() + ", " +
+                +antal +
+                ", " + getOrdrelinjePris() +
+                "kr.";
+    }
 }
+
+
+
